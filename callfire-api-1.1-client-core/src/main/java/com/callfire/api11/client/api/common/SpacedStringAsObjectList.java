014a8a155c825c7f211ffc560d9b882b879dc631
vmalinovskiy <vn.malinovskiy@gmail.com>
2016-08-29 11:51:42 +0300

    vmalinovskiy: fixed custom list deserializer

--- callfire-api-1.1-client-core/src/main/java/com/callfire/api11/client/api/common/SpacedStringAsObjectList.java
+++ callfire-api-1.1-client-core/src/main/java/com/callfire/api11/client/api/common/SpacedStringAsObjectList.java
@@ -2,15 +2,26 @@ package com.callfire.api11.client.api.common;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.deser.ContextualDeserializer;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class SpacedStringAsObjectList extends JsonDeserializer<List<Object>> {
+public class SpacedStringAsObjectList extends JsonDeserializer<List<?>> implements ContextualDeserializer {
+
+    private JavaType valueType;
+
+    @Override
+    public JsonDeserializer<List<?>> createContextual(DeserializationContext ctxt, BeanProperty property) throws JsonMappingException {
+        JavaType wrapperType = property.getType();
+        JavaType valueType = wrapperType.containedType(0);
+        SpacedStringAsObjectList deserializer = new SpacedStringAsObjectList();
+        deserializer.valueType = valueType;
+        return deserializer;
+    }
 
     @Override
     public List<Object> deserialize(JsonParser jsonParser, DeserializationContext context) throws IOException, JsonProcessingException {
@@ -20,7 +31,16 @@ public class SpacedStringAsObjectList extends JsonDeserializer<List<Object>> {
         if (stringToParse == null || stringToParse == "")
             return items;
 
-        return Arrays.asList((Object[]) stringToParse.split(" "));
+        Class paramClass = valueType.getRawClass();
+
+        if (paramClass.equals(Long.class)) {
+            for (String obj : stringToParse.split(" ")) {
+                items.add(Long.parseLong(obj));
+            }
+            return items;
+        } else {
+            return Arrays.asList((Object[]) stringToParse.split(" "));
+        }
     }
 
 }
